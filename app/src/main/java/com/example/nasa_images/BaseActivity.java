e3f0dc140b2597f29a16644dcf541bf563fb89a0
RioGuglielmelli <rio.guglielmelli@gmail.com>
2023-04-08 15:10:36 -0300

    Creation

---
+++ app/src/main/java/com/example/nasa_images/BaseActivity.java
@@ -0,0 +1,77 @@
+package com.example.nasa_images;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+abstract class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+
+        Toolbar myToolbar = (Toolbar)findViewById(R.id.tlB1);
+        setSupportActionBar(myToolbar);
+
+
+        drawerLayout = findViewById(R.id.drawerL);
+        ActionBarDrawerToggle Toggle = new ActionBarDrawerToggle(this, drawerLayout, myToolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(Toggle);
+        Toggle.syncState();
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item1:
+                Toast.makeText(this, "You clicked on item 1", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_item2:
+                Toast.makeText(this, "You clicked on item 2", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.nav_dad_joke:
+                startActivity(new Intent(this, DadJokeActivity.class));
+                break;
+            case R.id.nav_exit:
+                finishAffinity();
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+}
--- app/src/main/java/com/example/nasa_images/MainActivity.java
+++ app/src/main/java/com/example/nasa_images/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.nasa_images;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends BaseActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
